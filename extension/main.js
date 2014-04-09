aad825ec0f6b2fdee3f49df72017ae920ce5c59b
F.Ko-Ji <koji.fukunaga+github@gmail.com>
2014-04-09 23:37:54 +0900

    temporary commit

--- extension/main.js
+++ extension/main.js
@@ -1,11 +1,28 @@
-chrome.storage.local.get('options', function(items) {
-    var options;
+chrome.storage.local.get(['options', 'positions'], function(items) {
+    var options
+      , positions;
 
     if (typeof items.options !== 'undefined') {
         options = items.options;
     } else {
         options = ['PR:', 'AD:', '[PR]'];
     }
+
+    if (typeof items.positions !== 'undefined') {
+        positions = items.positions;
+    } else {
+        positions = [];
+        if (typeof items.options !== 'undefined') {
+            items.options.forEach(function() {
+                positions.push(0);
+            });
+        } else {
+            positions = [0, 0, 0];
+        }
+    }
+
+    console.log(options);
+    console.log(positions);
 
     document.body.addEventListener("DOMNodeInserted", function(e) {
         var el = e.target;
@@ -14,12 +31,13 @@ chrome.storage.local.get('options', function(items) {
         if (!el.id.match(/_main$/)) return;
         if (typeof el.dataset.title === 'undefined') return;
 
-        options.forEach(function(e) {
+        options.forEach(function(e, i) {
             if (e === '') {
                 return;
             }
 
-            if (el.dataset.title.toLowerCase().indexOf(e.toLowerCase()) === 0) {
+            if ((positions[i] === 0 && el.dataset.title.toLowerCase().indexOf(e.toLowerCase()) === 0) ||
+                (positions[i] === 1 && el.dataset.title.toLowerCase().indexOf(e.toLowerCase()) !== -1)) {
                 var event = el.ownerDocument.createEvent("MouseEvents");
                 event.initMouseEvent("click", true, true,
                                      el.ownerDocument.defaultView,
--- extension/options.js
+++ extension/options.js
@@ -1,59 +1,102 @@
+/**
+ * save settings to storage
+ */
 function save() {
-  var options = []
-  $('.option').each(function() {
-    options.push($(this).val());
-  });
-
-  chrome.storage.local.set({options: options}, function() {
-    $('#success_to_save').fadeIn(100, function() {
-      setTimeout(function() {
-        $('#success_to_save').fadeOut(500);
-      }, 2000);
+    var options = [];
+    $('.option').each(function() {
+        options.push($(this).val());
     });
-  });
+
+    var positions = [];
+    $('.position').each(function() {
+        // be number
+        positions.push($(this).val() * 1);
+    });
+
+    chrome.storage.local.set({
+        options: options,
+        positions: positions
+    }, function() {
+        $('#success_to_save').fadeIn(100, function() {
+            setTimeout(function() {
+            $('#success_to_save').fadeOut(500);
+        }, 2000);
+        });
+    });
 }
 
-function appendOption() {
-  var num = $('.option').length;
-  var $input = $('<input type="text">').attr('id', 'option_'+num).addClass('option');
-  $('<li></li>').append($input).appendTo('#options');
+/**
+ * create select menu
+ */
+function createSelectMenu(num) {
+    var $select = $('<select class="position"></select>').attr('id', 'position_'+num);
+
+    var positions = [
+        {text: 'start with', value: 0},
+        {text: 'include', value: 1}
+    ].forEach(function(el) {
+        $('<option></option>').val(el.value).text(el.text).appendTo($select);
+    });
+
+    return $select;
+}
+
+/**
+ * add option
+ */
+function addOptionToList() {
+    var num = $('.option').length;
+    var $input = $('<input type="text">').attr('id', 'option_'+num).addClass('option');
+    $('<li></li>')
+        .append($input)
+        .append(createSelectMenu(num))
+        .appendTo('#options');
 }
 
 $('#append').click(function() {
-  appendOption();
+    addOptionToList();
 });
 
-$('#back').click(function() {
-  if (confirm('Are you sure to initialize?')) {
-    initOptions();
-    chrome.storage.local.clear();
-  }
+$('#revert').click(function() {
+    if (confirm('Are you sure to initialize?')) {
+        initOptions();
+        chrome.storage.local.clear();
+    }
 });
 
 $('#save').click(function() {
-  save();
+    save();
 });
 
+/**
+ * initialize
+ */
 function initOptions() {
-  $('#options').empty();
-  ['PR:', 'AD:', '[PR]'].forEach(function(e, i) {
-    appendOption();
-    $('#option_'+i).val(e);
-  });
+    $('#options').empty();
+    ['PR:', 'AD:', '[PR]'].forEach(function(e, i) {
+        addOptionToList();
+        $('#option_'+i).val(e);
+    });
 }
 
 !function() {
-  chrome.storage.local.get('options', function(items) {
-    if (typeof items.options !== 'undefined') {
-      items.options.forEach(function(e, i) {
-        if (e == '') {
-            return;
+    chrome.storage.local.get(function(items) {
+        if (typeof items.options !== 'undefined') {
+            items.options.forEach(function(e, i) {
+                if (e == '') {
+                    return;
+                }
+                addOptionToList();
+                $('#option_'+i).val(e);
+            });
+
+            if (typeof items.positions !== 'undefined') {
+                items.positions.forEach(function(e, i) {
+                    $('#position_'+i).val(e);
+                });
+            }
+        } else {
+            initOptions();
         }
-        appendOption();
-        $('#option_'+i).val(e);
-      });
-    } else {
-      initOptions();
-    }
-  });
+    });
 }();
