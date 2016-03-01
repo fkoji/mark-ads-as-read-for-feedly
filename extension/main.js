70b530cf82a75a7c3a72feac019d489c08f20fa5
F.Ko-Ji <koji.fukunaga+github@gmail.com>
2016-03-02 05:00:20 +0900

    Bug fixed

--- extension/main.js
+++ extension/main.js
@@ -1,11 +1,17 @@
 chrome.storage.local.get(['options', 'positions'], function(items) {
-    var options
-      , positions;
+    'use strict';
+
+    const START_WITH = 0;
+    const INCLUDE    = 1;
+
+    var strs
+      , positions
+      , openedItemIds = {};
 
     if (typeof items.options !== 'undefined') {
-        options = items.options;
+        strs = items.options;
     } else {
-        options = ['PR:', 'AD:', '[PR]'];
+        strs = ['PR:', 'AD:', '[PR]'];
     }
 
     if (typeof items.positions !== 'undefined') {
@@ -14,21 +20,34 @@ chrome.storage.local.get(['options', 'positions'], function(items) {
         positions = [];
         if (typeof items.options !== 'undefined') {
             items.options.forEach(function() {
-                positions.push(0);
+                positions.push(START_WITH);
             });
         } else {
-            positions = [0, 0, 0];
+            positions = [START_WITH, START_WITH, START_WITH];
         }
     }
 
-    console.log('[MARF] %o', options);
-    console.log('[MARF] %o', positions);
+    //console.log('[MARF] %o', strs);
+    //console.log('[MARF] %o', positions);
 
     document.body.addEventListener("DOMNodeInserted", function(e) {
-        var el = e.target;
+        let el = e.target;
 
         if (el.nodeType !== 1) {
             return false;
+        }
+
+        if (typeof openedItemIds[el.id] !== 'undefined') {
+            let inlineFlameId = el.id.replace(/_main$/, '_inlineframe');
+
+            //console.log('[MARF] hidden %s', inlineFlameId);
+
+            // To prevent scroll to top, use setTimeout
+            setTimeout(function() {
+                document.getElementById(inlineFlameId).style.display = 'none';
+            }, 100);
+
+            return true;
         }
 
         if (!el.id.match(/_main$/)) {
@@ -39,29 +58,34 @@ chrome.storage.local.get(['options', 'positions'], function(items) {
             return false;
         }
 
-        options.forEach(function(e, i) {
-            if (e === '') {
+        strs.forEach(function(str, i) {
+            if (str === '') {
                 return;
             }
 
-            if ((positions[i] === 0 && el.dataset.title.toLowerCase().indexOf(e.toLowerCase()) === 0) ||
-                (positions[i] === 1 && el.dataset.title.toLowerCase().indexOf(e.toLowerCase()) !== -1)) {
-                var event = el.ownerDocument.createEvent("MouseEvents");
-                event.initMouseEvent("click", true, true,
+            let strIndex = el.dataset.title.toLowerCase().indexOf(str.toLowerCase());
+
+            // Matched
+            if (
+                (positions[i] === START_WITH && strIndex === 0)
+                || (positions[i] === INCLUDE && strIndex !== -1)
+            ) {
+                let event = el.ownerDocument.createEvent('MouseEvents');
+
+                openedItemIds[el.id] = true;
+
+                event.initMouseEvent('click', true, true,
                                      el.ownerDocument.defaultView,
                                      1, 0, 0, 0, 0,
                                      false, false, false, false,
                                      0, null);
-                el.dispatchEvent(event);
-   
-                //console.log('[MARF] %o', el.id);
 
                 setTimeout(function() {
-                    var inlineFlameId = el.id.replace(/_main_abstract$/, '_inlineframe');
-                    document.getElementById(inlineFlameId).style.display = 'none';
+                    //console.log('[MARF] %o', el.id);
+                    el.dispatchEvent(event);
+                    //console.log('[MARF] %s %d', str, i);
                 }, 100);
             }
-
         });
     }, false);
 });
